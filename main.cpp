0013b765c8d0c6e81cc89949cde7d29ac11b7298
jaewhyun <jaewhyun@gmail.com>
2017-07-20 18:53:49 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  mipsdisassembler
+//
+//  Created by Jae Won Hyun on 9/29/16.
+//  Copyright © 2016 Jae Won Hyun. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+#include "mipsdisassembler.h"
+
+using namespace std;
+
+int main()
+{
+    Disassembler a;
+    return 0;
+}
